d80981667cc7350f95d26840dd15556d56082629
AnilChomu Kumar Shukla <121477412+anil2003shukla@users.noreply.github.com>
2023-08-13 20:47:45 +0530

    Add files via upload

---
+++ random-gif/src/components/Random.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+const Random = () => {
+
+
+  const {gif, loading, fetchData} = useGif();
+
+
+  return (
+    <div className='w-1/2  bg-green-500 rounded-lg border border-black
+    flex flex-col items-center gap-y-5 mt-[15px]'>
+
+      <h1 className='mt-[15px] text-2xl underline uppercase font-bold'> A Random Gif</h1>
+
+    {
+        loading ? (<Spinner/>) : (<img src= {gif} width="450" />)
+    }
+
+      
+
+      <button onClick={() => fetchData()}
+      className="w-10/12 bg-white opacity-80 text-2xl font-bold py-2 rounded-lg mb=[30px]">
+
+       Generate
+
+      </button>
+
+    </div>
+  )
+}
+
+export default Random
